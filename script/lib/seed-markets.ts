7f833ba99b0799f9f980a4688e1d9e9f2c29f8fa
Tanishk Goyal <goyaltanishk02@gmail.com>
2026-01-08 23:49:44 +0400

    Update seed markets with current 2026 questions

--- script/lib/seed-markets.ts
+++ script/lib/seed-markets.ts
@@ -6,26 +6,17 @@ import { TEST_API_KEY } from './backend'
 
 // Seed market definitions
 const SEED_MARKETS = [
+    {
+    question: 'Will Tempo mainnet launch in 2026?',
+    resolutionDeadline: Math.floor(new Date('2027-1-1').getTime() / 1000), // End of 2026
+    assets: ['0x20C0000000000000000000000000000000000000'],
+  },
   {
-    question: 'Will ETH be above $5000 by end of Q1 2026?',
-    resolutionDeadline: Math.floor(Date.now() / 1000) + 90 * 24 * 60 * 60, // 90 days
+    question: 'Will stablecoin market cap exceed $1T by the end of 2026?',
+    resolutionDeadline: Math.floor(new Date('2027-1-1').getTime() / 1000), // End of 2026
     assets: ['0x20C0000000000000000000000000000000000000'], // PATH_USD
   },
-  {
-    question: 'Will BTC reach new ATH within 6 months of 2024 halving?',
-    resolutionDeadline: Math.floor(Date.now() / 1000) + 180 * 24 * 60 * 60, // 180 days
-    assets: ['0x20C0000000000000000000000000000000000000'],
-  },
-  {
-    question: 'Will Pectra upgrade deploy on mainnet by June 2025?',
-    resolutionDeadline: Math.floor(Date.now() / 1000) + 60 * 24 * 60 * 60, // 60 days
-    assets: ['0x20C0000000000000000000000000000000000000'],
-  },
-  {
-    question: 'Will stablecoin market cap exceed $200B by end of 2025?',
-    resolutionDeadline: Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60, // 365 days
-    assets: ['0x20C0000000000000000000000000000000000000'],
-  },
+
 ]
 
 export interface SeedMarketsOptions {
